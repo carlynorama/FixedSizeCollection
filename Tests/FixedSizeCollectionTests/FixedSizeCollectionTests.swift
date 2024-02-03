4af5f7a29cf436168ac6b72540e5cb5ab6cec81a
carlynorama <carlynorama@gmail.com>
2024-02-03 07:39:22 -0800

    Created tests

--- Tests/FixedSizeCollectionTests/FixedSizeCollectionTests.swift
+++ Tests/FixedSizeCollectionTests/FixedSizeCollectionTests.swift
@@ -2,6 +2,45 @@ import XCTest
 @testable import FixedSizeCollection
 
 final class FixedSizeCollectionTests: XCTestCase {
+    func testBasicInitWithDefault() {
+        let testCollection = FixedSizeCollection(4, default: 12) { [1, 2, 3] }
+        XCTAssertEqual(testCollection[0], 1, "collection 0 incorrect")
+        XCTAssertEqual(testCollection[1], 2, "collection 1 incorrect")
+        XCTAssertEqual(testCollection[2], 3, "collection 2 incorrect")
+        XCTAssertEqual(testCollection[3], 12, "collection 3 incorrect")
+        
+    }
+    
+    func testBasicInitWithOptional() {
+        let testCollection = FixedSizeCollection<Int?>(5, default: nil) { [1, 2, 3] }
+        XCTAssertEqual(testCollection[0], 1, "collection 0 incorrect")
+        XCTAssertEqual(testCollection[1], 2, "collection 1 incorrect")
+        XCTAssertEqual(testCollection[2], 3, "collection 2 incorrect")
+        XCTAssertEqual(testCollection[3], nil, "collection 3 incorrect")
+        XCTAssertEqual(testCollection[4], nil, "collection 3 incorrect")
+    }
+    
+    func testMeasureAccess() {
+        let baseArray = [1, 2, 3, 7]
+        let testCollection = FixedSizeCollection<Int?>(baseArray.count, default: nil) { baseArray }
+        measure {
+            for i in 0..<testCollection.count {
+                XCTAssertEqual(testCollection[i], baseArray[i], "collection did not retrieve expected value")
+            }
+        }
+    }
+    
+    func testUpdate() {
+        var testCollection = FixedSizeCollection<Int>(5, default: 0)
+        measure {
+            for i in 0..<testCollection.count {
+                let newVar = Int.random(in: 0...100)
+                testCollection[i] = newVar
+                XCTAssertEqual(testCollection[i], newVar, "collection did not update correctly")
+            }
+        }
+    }
+    
     func testExample() throws {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
