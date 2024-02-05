c640a7053e6a30f5f0205ea9c60f4a80a035de61
carlynorama <carlynorama@gmail.com>
2024-02-05 11:39:54 -0800

    back away...

--- Sources/FixedSizeCollection/Operators.swift
+++ Sources/FixedSizeCollection/Operators.swift
@@ -11,19 +11,27 @@
 //===----------------------------------------------------------------------===//
 
 
+//TODO: try to get the & working.
+//TODO: try to get something that will something that can work for C const*
+
+//STATUS: On hold.
+
+
 //https://github.com/apple/swift/blob/b5632dece23a9d11bc3999a2d221b5f14df80925/docs/ABI/Mangling.rst#L1072
 //https://github.com/apple/swift-evolution/blob/5ba0de00fb406f81aad35c25bc3a7036971e3445/proposals/0035-limit-inout-capture.md?plain=1#L26
-
 //https://developer.apple.com/documentation/swift/calling-functions-with-pointer-parameters
 
 
-prefix operator ‡
-extension FixedSizeCollection {
-    
-    //⇧⌥7  e.g. ⌥&
-//    public static prefix func ‡(fsc: Self) -> UnsafePointer<[Element]> {
+//prefix operator ‡
+//prefix operator ‡   //can't do custom &
+//extension FixedSizeCollection {
+//    
+//    //⇧⌥7  e.g. ⌥&
+////    public static prefix func ‡(fsc: Self) -> ???? {
+////
+////    }
+//    
+//    //TODO: how does array get &, find it.
 //        
-//    }
-        
-    
-}
+//    
+//}
