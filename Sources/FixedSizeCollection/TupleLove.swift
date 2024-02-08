b0c3513087bbda0e67e4c6b6b498ea3749fc5db9
carlynorama <carlynorama@gmail.com>
2024-02-07 17:34:52 -0800

    _get(type, tuple) tested, works

--- Sources/FixedSizeCollection/TupleLove.swift
+++ Sources/FixedSizeCollection/TupleLove.swift
@@ -12,19 +12,8 @@ internal extension FixedSizeCollection {
     
     //MARK: Utilities
     
-    //Can't use parameter packs to enforce single type conformance yet.
-    static func newRegularTuple<each T>(_ item: repeat each T) -> (repeat each T) {
-        //return (repeat each item as? E) for example, not a thing.
-        return (repeat each item)
-    }
-    
-    //Not a thing yet...
-    // static func newRegularTuple<each T, E>(ofType:E.Type, _ item: repeat each T) -> (repeat each T) {
-    //     return (repeat each item as? E)
-    // }
-    
     @inlinable
-    static func _confirmSizeOfTuple<U>(tuple:U, expectedCount:N? = nil) throws -> N {
+    static func _confirmSizeOfTuple<each T>(tuple:(repeat each T), expectedCount:N? = nil) throws -> N {
         let count = Mirror(reflecting: tuple).children.count
         if expectedCount != nil, expectedCount != count  {
             throw FSCError.unknownError(message: "tuple's children and tuple's expected count not the same.")
@@ -32,25 +21,11 @@ internal extension FixedSizeCollection {
         return count
     }
     
-    //TODO: Compare speed. This version checks type.
-    //The other is a pinky swear from the client.
-    @inlinable
-    static func _tupleAsArray<U, T>(tuple:U, isType:T.Type) throws -> [T] {
-        var newArray:[T] = []
-        Mirror(reflecting: tuple).children.forEach { child in
-            if let newValue = child.value as? T {
-                newArray.append(newValue)
-            }
-        }
-        return newArray
-    }
-    
-    
-    //MARK: Getting Out
-    
+        
+    //MARK: Getting Values Out
     //Less safe in the case that the memory is not in fact bound to this.
     @inlinable
-    static func _getFixedSizeCArrayAssumed<T, R>(source:T, boundToType:R.Type, withCount count:N? = nil) -> [R] {
+    static func _getFixedSizeCArrayAssumed<T, R>(source:T, boundToType:R.Type) -> [R] {
         return Swift.withUnsafeBytes(of: source) { (rawPointer) -> [R] in
             let bufferPointer = rawPointer.assumingMemoryBound(to: boundToType)
             return [R](bufferPointer)
@@ -64,16 +39,36 @@ internal extension FixedSizeCollection {
     //            rawPointer.baseAddress?.load(as: [R].self)
     //        }
     //    }
+
+        //Can't use parameter packs to enforce single type conformance yet.
+    static func newRegularTuple<each T>(_ item: repeat each T) -> (repeat each T) {
+        //return (repeat each item as? E) for example, not a thing.
+        return (repeat each item)
+    }
+    
+    //TODO: Compare speed. This version checks type.
+    //The currently use one other is a pinky swear from the client
+    //keep an eye on https://github.com/apple/swift/pull/70227
+    static func _get<T, R>(valuesBoundTo:R.Type, from tuple: T) -> [R] {
+            var newArray:[R] = []
+            Mirror(reflecting: tuple).children.forEach { child in
+            if let newValue = child.value as? R {
+                newArray.append(newValue)
+            }
+        }
+        return newArray
+    }
     
     //MARK: Putting Back
+    //Uses withMemoryRebound. Be sure target's memory is in fact bound to that.
     @inlinable
-    func _loadIntoTuple<U, T>(tuple: inout U, count:N, type:T.Type) throws {
+    func _loadIntoTuple<T, R>(tuple: inout T, count:N, type:R.Type) throws {
         precondition(count == self.count)
         precondition(type == Element.self)
-        precondition(MemoryLayout.size(ofValue: tuple) == MemoryLayout<T>.stride * count)
+        precondition(MemoryLayout.size(ofValue: tuple) == MemoryLayout<R>.stride * count)
         
         Swift.withUnsafeMutablePointer(to: &tuple) { tuplePointer in
-            precondition(Int(bitPattern: tuplePointer).isMultiple(of: MemoryLayout<T>.alignment))
+            precondition(Int(bitPattern: tuplePointer).isMultiple(of: MemoryLayout<R>.alignment))
             tuplePointer.withMemoryRebound(to: Element.self, capacity: count) { reboundPointer in
                 let bufferPointer = UnsafeMutableBufferPointer(start: UnsafeMutablePointer(reboundPointer), count: count)
                 for i in stride(from: bufferPointer.startIndex, to: bufferPointer.endIndex, by: 1) {
@@ -86,19 +81,21 @@ internal extension FixedSizeCollection {
     
     //YOLO transfer.
     //Don't use this. Especially for anything more complicated than a Int, which you might get away with, but I didn't say that.
+    //This one also doesn't accept upgrading to variadic generics.
+    //TODO, figure out why but give it a minute because should use this anyway.
     @inlinable
-    func _memCopyToTuple<U>(tuple: inout U, count:Int, type:N.Type) throws {
+    func _memCopyToTuple<U, R>(tuple: inout U, count:Int, type:R.Type) throws {
+    //func _memCopyToTuple<each T, R>(tuple: inout (repeat each T), count:Int, type:R.Type) throws {
         precondition(count == self.count)
         precondition(type == Element.self)
-        precondition(MemoryLayout.size(ofValue: tuple) == MemoryLayout<N>.stride * count)
-        precondition(MemoryLayout.size(ofValue: tuple) == MemoryLayout<N>.stride * self.count)
+        precondition(MemoryLayout.size(ofValue: tuple) == MemoryLayout<R>.stride * count)
+        precondition(MemoryLayout.size(ofValue: tuple) == MemoryLayout<R>.stride * self.count)
         
         try Swift.withUnsafeMutablePointer(to: &tuple) { tuplePointer in
-            precondition(Int(bitPattern: tuplePointer).isMultiple(of: MemoryLayout<N>.alignment))
+            precondition(Int(bitPattern: tuplePointer).isMultiple(of: MemoryLayout<R>.alignment))
             let _ = try self.withUnsafeBufferPointer { bufferPointer in
-                memcpy(tuplePointer, bufferPointer?.baseAddress, count * MemoryLayout<N>.stride)
+                memcpy(tuplePointer, bufferPointer?.baseAddress, count * MemoryLayout<R>.stride)
             }
-            
         }
     }
     
--- Tests/FixedSizeCollectionTests/TestingWithC.swift
+++ Tests/FixedSizeCollectionTests/TestingWithC.swift
@@ -16,10 +16,18 @@ final class WithCTests: XCTestCase {
     //MARK: CArrays
     
     func testTupleEnforcement() throws {
-        let tmp_array = try FixedSizeCollection<Int32>._tupleAsArray(tuple: fsc_int32_array, isType: Int32.self)
+        let tmp_array = FixedSizeCollection<Int32>._get(valuesBoundTo: Int32.self, from: fsc_int32_array)
         
-        FixedSizeCollection(tmp_array)
-       
+        let tC = FixedSizeCollection(tmp_array)
+        
+        XCTAssertEqual(tC[0], fsc_int32_array.0, "0: no match.")
+        XCTAssertEqual(tC[1], fsc_int32_array.1, "1: no match.")
+        XCTAssertEqual(tC[2], fsc_int32_array.2, "2: no match.")
+        XCTAssertEqual(tC[3], fsc_int32_array.3, "3: no match.")
+        XCTAssertEqual(tC[4], fsc_int32_array.4, "4: no match.")
+        XCTAssertEqual(tC[5], fsc_int32_array.5, "5: no match.")
+        XCTAssertEqual(tC[6], fsc_int32_array.6, "6: no match.")
+        
         
     }
     
@@ -29,26 +37,26 @@ final class WithCTests: XCTestCase {
         
         let tmp_Array = FixedSizeCollection<Int32>._getFixedSizeCArrayAssumed(source: fsc_int32_array, boundToType: Int32.self)
         let tC = FixedSizeCollection.makeFixedSizeCollection(count:tmp_Array.count , fillValue: 0, values: tmp_Array)
-        print(tC.count)
-//
-        XCTAssertEqual(tC[0], fsc_int32_array.0, "collection did not retrieve expected value")
-        XCTAssertEqual(tC[1], fsc_int32_array.1, "collection did not retrieve expected value")
-        XCTAssertEqual(tC[2], fsc_int32_array.2, "collection did not retrieve expected value")
-        XCTAssertEqual(tC[3], fsc_int32_array.3, "collection did not retrieve expected value")
-        XCTAssertEqual(tC[4], fsc_int32_array.4, "collection did not retrieve expected value")
-        XCTAssertEqual(tC[5], fsc_int32_array.5, "collection did not retrieve expected value")
-        XCTAssertEqual(tC[6], fsc_int32_array.6, "collection did not retrieve expected value")
+        //print(tC.count)
+        //
+        XCTAssertEqual(tC[0], fsc_int32_array.0, "0: no match.")
+        XCTAssertEqual(tC[1], fsc_int32_array.1, "1: no match.")
+        XCTAssertEqual(tC[2], fsc_int32_array.2, "2: no match.")
+        XCTAssertEqual(tC[3], fsc_int32_array.3, "3: no match.")
+        XCTAssertEqual(tC[4], fsc_int32_array.4, "4: no match.")
+        XCTAssertEqual(tC[5], fsc_int32_array.5, "5: no match.")
+        XCTAssertEqual(tC[6], fsc_int32_array.6, "6: no match.")
         
         
         let testCT = FixedSizeCollection(asCopyOfTuple:fsc_int32_array, ofType: Int32.self)
         
-        XCTAssertEqual(testCT[0], fsc_int32_array.0, "collection did not retrieve expected value")
-        XCTAssertEqual(testCT[1], fsc_int32_array.1, "collection did not retrieve expected value")
-        XCTAssertEqual(testCT[2], fsc_int32_array.2, "collection did not retrieve expected value")
-        XCTAssertEqual(testCT[3], fsc_int32_array.3, "collection did not retrieve expected value")
-        XCTAssertEqual(testCT[4], fsc_int32_array.4, "collection did not retrieve expected value")
-        XCTAssertEqual(testCT[5], fsc_int32_array.5, "collection did not retrieve expected value")
-        XCTAssertEqual(testCT[6], fsc_int32_array.6, "collection did not retrieve expected value")
+        XCTAssertEqual(testCT[0], fsc_int32_array.0, "0: no match")
+        XCTAssertEqual(testCT[1], fsc_int32_array.1, "1: no match")
+        XCTAssertEqual(testCT[2], fsc_int32_array.2, "2: no match")
+        XCTAssertEqual(testCT[3], fsc_int32_array.3, "3: no match")
+        XCTAssertEqual(testCT[4], fsc_int32_array.4, "4: no match")
+        XCTAssertEqual(testCT[5], fsc_int32_array.5, "5: no match")
+        XCTAssertEqual(testCT[6], fsc_int32_array.6, "6: no match")
         
     }
     
@@ -58,26 +66,26 @@ final class WithCTests: XCTestCase {
         let tC = FixedSizeCollection(baseArray)
         try tC.copyIntoTupleDestination(tuple:&fsc_uint8_array)
         
-        XCTAssertEqual(baseArray[0], fsc_uint8_array.0, "collection did not retrieve expected value")
-        XCTAssertEqual(baseArray[1], fsc_uint8_array.1, "collection did not retrieve expected value")
-        XCTAssertEqual(baseArray[2], fsc_uint8_array.2, "collection did not retrieve expected value")
-        XCTAssertEqual(baseArray[3], fsc_uint8_array.3, "collection did not retrieve expected value")
-        XCTAssertEqual(baseArray[4], fsc_uint8_array.4, "collection did not retrieve expected value")
-        XCTAssertEqual(baseArray[5], fsc_uint8_array.5, "collection did not retrieve expected value")
-        XCTAssertEqual(baseArray[6], fsc_uint8_array.6, "collection did not retrieve expected value")
-        XCTAssertEqual(baseArray[7], fsc_uint8_array.7, "collection did not retrieve expected value")
-        XCTAssertEqual(baseArray[8], fsc_uint8_array.8, "collection did not retrieve expected value")
-        XCTAssertEqual(baseArray[9], fsc_uint8_array.9, "collection did not retrieve expected value")
-        XCTAssertEqual(baseArray[10], fsc_uint8_array.10, "collection did not retrieve expected value")
-        XCTAssertEqual(baseArray[11], fsc_uint8_array.11, "collection did not retrieve expected value")
-        XCTAssertEqual(baseArray[12], fsc_uint8_array.12, "collection did not retrieve expected value")
-        XCTAssertEqual(baseArray[13], fsc_uint8_array.13, "collection did not retrieve expected value")
-        XCTAssertEqual(baseArray[14], fsc_uint8_array.14, "collection did not retrieve expected value")
-        XCTAssertEqual(baseArray[15], fsc_uint8_array.15, "collection did not retrieve expected value")
-        XCTAssertEqual(baseArray[16], fsc_uint8_array.16, "collection did not retrieve expected value")
-        XCTAssertEqual(baseArray[17], fsc_uint8_array.17, "collection did not retrieve expected value")
-        XCTAssertEqual(baseArray[18], fsc_uint8_array.18, "collection did not retrieve expected value")
-        XCTAssertEqual(baseArray[26], fsc_uint8_array.26, "collection did not retrieve expected value")
+          XCTAssertEqual(baseArray[0], fsc_uint8_array.0, "00: no match")
+          XCTAssertEqual(baseArray[1], fsc_uint8_array.1, "01: no match")
+          XCTAssertEqual(baseArray[2], fsc_uint8_array.2, "02: no match")
+          XCTAssertEqual(baseArray[3], fsc_uint8_array.3, "03: no match")
+          XCTAssertEqual(baseArray[4], fsc_uint8_array.4, "04: no match")
+          XCTAssertEqual(baseArray[5], fsc_uint8_array.5, "05: no match")
+          XCTAssertEqual(baseArray[6], fsc_uint8_array.6, "06: no match")
+          XCTAssertEqual(baseArray[7], fsc_uint8_array.7, "07: no match")
+          XCTAssertEqual(baseArray[8], fsc_uint8_array.8, "08: no match")
+          XCTAssertEqual(baseArray[9], fsc_uint8_array.9, "09: no match")
+        XCTAssertEqual(baseArray[10], fsc_uint8_array.10, "10: no match")
+        XCTAssertEqual(baseArray[11], fsc_uint8_array.11, "11: no match")
+        XCTAssertEqual(baseArray[12], fsc_uint8_array.12, "12: no match")
+        XCTAssertEqual(baseArray[13], fsc_uint8_array.13, "13: no match")
+        XCTAssertEqual(baseArray[14], fsc_uint8_array.14, "14: no match")
+        XCTAssertEqual(baseArray[15], fsc_uint8_array.15, "15: no match")
+        XCTAssertEqual(baseArray[16], fsc_uint8_array.16, "16: no match")
+        XCTAssertEqual(baseArray[17], fsc_uint8_array.17, "17: no match")
+        XCTAssertEqual(baseArray[18], fsc_uint8_array.18, "18: no match")
+        XCTAssertEqual(baseArray[26], fsc_uint8_array.26, "26: no match")
     }
     
     
